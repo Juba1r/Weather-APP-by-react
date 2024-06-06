7f4f30a288a3c4814b7e81951a87630ea9f0ccaf
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-06-06 14:49:05 +0600

    update

---
+++ Weather app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Weather from './Components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      
+      <Weather/>
+     
+    
+    
+    </div>
+  )
+}
+
+export default App
---
+++ Weather app/src/Components/Weather.jsx
@@ -0,0 +1,16 @@
+import './Weather.css'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='search' />
+
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Weather
