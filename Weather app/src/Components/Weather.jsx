ef694302e130ce1dcebe4f515c510e0c7626fde9
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-06-06 15:07:08 +0600

    update

--- Weather app/src/Components/Weather.jsx
+++ Weather app/src/Components/Weather.jsx
@@ -1,5 +1,12 @@
 import './Weather.css'
 import search_icon from '../Assets/search.png'
+import clear_icon from '../Assets/clear.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
 
 const Weather = () => {
   return (
@@ -9,6 +16,9 @@ const Weather = () => {
             <img src={search_icon} alt="" />
 
         </div>
+        <img src={clear_icon} alt="" className='weather-icon'/>
+        <p className='temperature'>16°c</p>
+        <p className='location'>London</p>
 
         
     </div>
