39bca32705bd78943532f58c79138cedc22e461c
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-06-06 15:01:24 +0600

    update

--- Weather app/src/Components/Weather.jsx
+++ Weather app/src/Components/Weather.jsx
@@ -1,10 +1,12 @@
 import './Weather.css'
+import search_icon from '../Assets/search.png'
 
 const Weather = () => {
   return (
     <div className='weather'>
         <div className="search-bar">
             <input type="text" placeholder='search' />
+            <img src={search_icon} alt="" />
 
         </div>
 
